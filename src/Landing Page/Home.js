f4d2899796d522fab0c562bfbd0d02e07d503fe8
jassmin-herr <jassminfernandez@gmail.com>
2022-01-02 22:07:45 -0600

    'adds responsive footer w/ icons'

--- src/Landing Page/Home.js
+++ src/Landing Page/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Header } from "../components/Header";
+import { Footer } from "../components/Footer/Footer";
 import Style from "./home.css";
 
 const Home = () => {
