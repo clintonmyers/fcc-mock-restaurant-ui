861832ba35400d9945a6abce6f3a5fd23aec80ea
Clint Myers <clint.r.myers@gmail.com>
2022-01-07 01:37:34 -0600

    New Menu and Order components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.css";
 import { useApiGet, TApiResponse } from "./hooks/useApiHook";
 import Home from "./Landing Page/Home";
 import Restaurants from "./Restaurants/Restaurants";
+import Menu from "./Restaurants/Menu/Menu";
+import Order from "./Restaurants/Order/Order";
 
 function App() {
   // call to the hook
@@ -19,10 +21,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Restaurants />}>
           <Route index element={<Home />} />
-          {/* <Route path="/menu" element={<Menu />} /> */}
-          {/* <Route path="/order" element={<Order />} /> */}
-          <Route path="menu" element="Menu" />
-          <Route path="order" element="Order" />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
         </Route>
         <Route path="/admin" element="ADMIN" />
       </Routes>
