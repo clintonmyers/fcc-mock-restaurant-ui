5a90e210e80f8eed42cfad271cf2d5bccc100249
Scott Thompson <sthompson082290@gmail.com>
2022-01-07 11:18:38 -0600

    added carousel of images on landing page and our story text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 //import logo from "./logo.svg"; i dont think we need this?
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
 import { useApiGet, TApiResponse } from "./hooks/useApiHook";
 import Home from "./Landing Page/Home";
 
--- src/Landing Page/Home.js
+++ src/Landing Page/Home.js
@@ -1,31 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import { Header } from "../components/Header";
 import Style from "./home.css";
+import Slideshow from "./Slideshow";
 
 const Home = () => {
   return (
     <div className="home-container">
+      <Header />
       <div className="breakfastImg-container">
         <img
           id="homeBfast-img"
           src="https://media.istockphoto.com/photos/breakfast-with-bacon-eggs-pancakes-and-toast-picture-id533645537?k=20&m=533645537&s=612x612&w=0&h=KJXCpAo9XQvMI_djcnRMSsz_Y7OGS6z3-8VThxWyR0Q="
           alt="Breakfast Plate"
         ></img>
-        <div className="order-container">
-          <h2>
-            Order Your <b id="breakfast-font">BREAKFAST </b>Online Now
-          </h2>
-          <img
-            id="car-img"
-            src="https://images.vexels.com/media/users/3/155413/isolated/preview/02fa4279f1759a65b62bafe7caf5be32-suv-car-front-view-silhouette.png"
-            alt="Car Pickup Logo"
-          ></img>
-          <p>
-            <b>
-              <em>for delivery or pickup!</em>
-            </b>
-          </p>
-        </div>
+      </div>
+      <div className="order-container">
+        <h2>
+          Order Your <b id="breakfast-font">BREAKFAST </b>Online Now
+        </h2>
+        <img
+          id="car-img"
+          src="https://images.vexels.com/media/users/3/155413/isolated/preview/02fa4279f1759a65b62bafe7caf5be32-suv-car-front-view-silhouette.png"
+          alt="Car Pickup Logo"
+        ></img>
+        <p>
+          <b>
+            <em>for delivery or pickup!</em>
+          </b>
+        </p>
+        <a href="" target="_blank" id="placeOrder">
+          <b>Place your order</b>
+        </a>
+      </div>
+      <div className="divider" id="dividerTwo"></div>
+      <div className="slideshow-container">
+        <Slideshow />
+      </div>
+      <div className="story-container">
+        <h1>Our food story</h1>
+        <p>
+          Our breakfista's carefully craft a comfy collection of breakfast and
+          lunch delectibles that help melt all of your troubles away!
+        </p>
       </div>
     </div>
   );
